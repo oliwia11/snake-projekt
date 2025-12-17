9bef36563a0801f13a606a8edefafb3c28213888
Zuzanna Pomian <zuzannap91@gmail.com>
2025-12-17 13:58:06 +0100

    dodanie logiki gry- ogon, kolizje, owoce i pętla główna
    Refactor game logic to include tail rendering and collision detection.

--- game.cpp
+++ game.cpp
@@ -57,8 +57,16 @@ void Draw() {
                 cout << "O";
             else if (i == fruitY && j == fruitX)
                 cout << "@";
-            else
-                cout << " ";
+            else { 				//rysowanie ogona
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print) cout << " ";
+            }
 
             if (j == WIDTH - 1) cout << "#";
         }
@@ -92,9 +100,46 @@ void Input() {  //kontrala oblugi klawiatury i poruszania sie
         }
     }
 }
-
-int main(){
-	Setup();
-	Draw();
+void Logic() { // logika gry - ruch ogona, kolizje, owoce
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x; // pierwszy element ogona to pozycja glowy
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    } // ruch glowy w zaleznosci od kierunku
+    switch (dir) {
+    case LEFT:  x--; break;
+    case RIGHT: x++; break;
+    case UP:    y--; break;
+    case DOWN:  y++; break;
+    default:    break;
+    }
+    if (x >= WIDTH || x < 0 || y >= HEIGHT || y < 0) gameOver = true; // kolizje ze scianami
+    for (int i = 0; i < nTail; i++) if (tailX[i] == x && tailY[i] == y) gameOver = true; //kolizje z ogonem
+    if (x == fruitX && y == fruitY) { // jedzenie owoca
+        score += 10;
+        nTail++; // wydluzenie owoca
+        fruitX = rand() % WIDTH; // nowy owoc
+        fruitY = rand() % HEIGHT;
+    }
 }
+
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        Sleep(50); //kontrola predkosci weza
+    }
+    return 0;
+}
+
 
